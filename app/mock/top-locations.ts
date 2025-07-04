cf5eff1cf7c8d13811fe7403dc3f04d74ccfcb4c
gguevara112 <guevara.guillermo.g@gmail.com>
2025-07-04 13:34:32 -0400

    listo

--- app/mock/top-locations.ts
+++ app/mock/top-locations.ts
@@ -1,44 +1,28 @@
 import {
   BookOpen,
-  FlaskConical,
-  BriefcaseMedical,
+  Coffee,
+  Bath,
   Users,
   Navigation,
-  Footprints,
+  GraduationCap,
 } from "lucide-react";
 
 const topLocations = [
   {
-    name: "Laboratorio 1",
-    icon: FlaskConical,
-    colors: "bg-blue-100 text-blue-700 dark:bg-blue-700 dark:text-blue-100",
-  },
-  {
-    name: "Laboratorio 2",
-    icon: FlaskConical,
-    colors:
-      "bg-emerald-100 text-emerald-700 dark:bg-emerald-700 dark:text-emerald-100",
-  },
-  {
-    name: "Oficina 1",
-    icon: BriefcaseMedical,
-    colors: "bg-red-100 text-red-700 dark:bg-red-700 dark:text-red-100",
-  },
-  {
-    name: "Sala de Estudio",
-    icon: BookOpen,
-    colors:
-      "bg-purple-100 text-purple-700 dark:bg-purple-700 dark:text-purple-100",
-  },
-  {
-    name: "Aula 101",
-    icon: Users,
+    name: "Coffe Break",
+    icon: Coffee,
     colors: "bg-amber-100 text-amber-700 dark:bg-amber-700 dark:text-amber-100",
   },
   {
-    name: "Pasillo Principal",
-    icon: Navigation,
-    colors: "bg-slate-100 text-slate-700 dark:bg-slate-700 dark:text-slate-100",
+    name: "Sala de estudio 1-F",
+    icon: BookOpen,
+    colors: "bg-green-100 text-green-700 dark:bg-green-700 dark:text-green-100",
+  },
+
+  {
+    name: "Baño",
+    icon: Bath,
+    colors: "bg-cyan-100 text-cyan-700 dark:bg-cyan-700 dark:text-cyan-100",
   },
 ];
 
